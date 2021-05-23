edae7e674db78d422ca684a2c61c25d458346d55
ykarkhele <ykarkhele@host.docker.internal>
2021-05-23 18:49:50 +0530

    removed comment

--- src/com/demo/testsuite/ActionClass.java
+++ src/com/demo/testsuite/ActionClass.java
@@ -1,10 +1,5 @@
 package com.demo.testsuite;
 
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.Keyboard;
 import org.testng.annotations.Test;
 
 import com.demo.common.BaseTestCase;
