6bf80db09c07768e5d1f595c87e466b0a0e1e1fa
ykarkhele <ykarkhele@host.docker.internal>
2021-05-25 09:06:32 +0530

    Changes from DemoBranch

--- src/com/demo/testsuite/GitTestClass.java
+++ src/com/demo/testsuite/GitTestClass.java
@@ -3,7 +3,8 @@ package com.demo.testsuite;
 public class GitTestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("test3");
+		System.out.println("test4");
 
 	}
 
