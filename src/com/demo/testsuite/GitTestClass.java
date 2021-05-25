f7f25af066518138b6a55d36313706a1830a06dc
ykarkhele <ykarkhele@host.docker.internal>
2021-05-25 09:16:54 +0530

    changes made

--- src/com/demo/testsuite/GitTestClass.java
+++ src/com/demo/testsuite/GitTestClass.java
@@ -4,8 +4,8 @@ public class GitTestClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("test1");
-		System.out.println("test2");
+		System.out.println("new");
+		System.out.println("new1");
 
 
 	}
